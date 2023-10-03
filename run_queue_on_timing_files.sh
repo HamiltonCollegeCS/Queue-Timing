7bbb4cbdffa545d671823e1b10c07b986d7402df
Mark Bailey <mwbmwb@users.noreply.github.com>
2023-10-03 14:00:58 -0400

    Update run_queue_on_timing_files.sh

--- run_queue_on_timing_files.sh
+++ run_queue_on_timing_files.sh
@@ -1,4 +1,4 @@
-for c in timing/queue2to*.txt; do
+for c in Queue-Timing/queue2to*.txt; do
 	echo -e "\n==============================================\n"
 	echo -en "Running ${c}...\n"
 	./queue "${c}"
